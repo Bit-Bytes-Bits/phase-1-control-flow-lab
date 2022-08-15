2c8ef6b6a1f2c15117a1693d81a2ca3f3d7eeefc
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-08-15 22:52:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// 1. If else-if statement
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  if (ride <= 400){
+    return 'This one is on me!'
+  }
+  else if (ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(ride > 2500){
+    return 'No can do.'
+  }
 }
+scuberGreetingForFeet(199)
 
-function switchOnCharmFromTip(){
+// 2. Ternary operator
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let result = (city === 'NYC')? 'Ok, sounds good.' : 'No go.';
+  return result
 }
+ternaryCheckCity("NYC")
+
+// 3. Switch statement
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+  }
+}
+switchOnCharmFromTip("generous");
